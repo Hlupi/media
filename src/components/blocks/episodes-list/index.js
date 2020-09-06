273cda5178f8732e5c0952a9708c8e549e156c98
Julia <yu.ashikhmina@gmail.com>
2020-09-06 19:34:16 +0200

    extract episode card to a fragment

--- src/components/blocks/episodes-list/index.js
+++ src/components/blocks/episodes-list/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { episodesData } from '../../../data'
+import Card from '../../fragments/card'
 import Arrow from '../../fragments/icons/arrow'
 
 const Wrapper = styled.div`
@@ -13,65 +14,6 @@ const List = styled.ul`
   margin-bottom: 14px;
   display: flex;
   transition: transform .35s ease-out;
-`
-
-const Card = styled.li`
-  flex-basis: 200px;
-  flex-grow: 0;
-  flex-shrink: 0;
-  margin-right: 28px;
-`
-
-const Thumb = styled.div`
-  margin-bottom: 20px;
-  height: 134px;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  position: relative;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background: #000000;
-    z-index: 1;
-    opacity: ${({ selected }) => selected ? '0' : '0.6'};
-    transition: opacity .35s ease-out;
-  }
-  &:hover {
-    &:before {
-       opacity: 0;
-    }
-  }
-`
-
-const Number = styled.span`
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  width: 30px;
-  height: 30px;
-  background: #fff;
-  color: #000;
-  font-family: 'Helvetica Neue Bold';
-  font-size: 16px;
-  line-height: 19px;
-  position: relative;
-  z-index: 2;
-`
-
-const H3 = styled.h3`
-  margin-bottom: 10px;
-  font-family: 'Helvetica Neue Bold';
-  font-size: 15px;
-  line-height: 18px;
-`
-
-const P = styled.p`
-  font-size: 13px;
-  line-height: 15px;
 `
 
 const BContainer = styled.div`
@@ -105,13 +47,13 @@ const EpisodesList = React.forwardRef(({ episodes = [], select, selected }, ref)
   const renderEpisodes = episodes && episodes.map((episode, i) => {
     const { Episode, Title } = episode
     return (
-      <Card key={i} onClick={() => select(i)}>
-        <Thumb selected={i === selected} style={{ backgroundImage: `url("/img/${episodesData[i].image}")` }}>
-          <Number>{Episode}</Number>
-        </Thumb>
-        <H3>{Title}</H3>
-        <P>{episodesData[i].plot}</P>
-      </Card>
+      <Card key={i}
+        onClick={() => select(i)}
+        selected={i === selected}
+        image={episodesData[i].image}
+        episode={Episode}
+        title={Title}
+        plot={episodesData[i].plot} />
     )
   })
 
---
+++ src/components/fragments/card/index.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.li`
+  flex-basis: 200px;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-right: 28px;
+`
+
+const Thumb = styled.div`
+  margin-bottom: 20px;
+  height: 134px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #000000;
+    z-index: 1;
+    opacity: ${({ selected }) => selected ? '0' : '0.6'};
+    transition: opacity .35s ease-out;
+  }
+  &:hover {
+    &:before {
+       opacity: 0;
+    }
+  }
+`
+
+const Number = styled.span`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  width: 30px;
+  height: 30px;
+  background: #fff;
+  color: #000;
+  font-family: 'Helvetica Neue Bold';
+  font-size: 16px;
+  line-height: 19px;
+  position: relative;
+  z-index: 2;
+`
+
+const H3 = styled.h3`
+  margin-bottom: 10px;
+  font-family: 'Helvetica Neue Bold';
+  font-size: 15px;
+  line-height: 18px;
+`
+
+const P = styled.p`
+  font-size: 13px;
+  line-height: 15px;
+`
+
+
+const Card = ({ onClick, selected, image, episode, title, plot}) => {
+  return (
+    <Container onClick={onClick}>
+      <Thumb selected={selected} style={{ backgroundImage: `url("/img/${image}")` }}>
+        <Number>{episode}</Number>
+      </Thumb>
+      <H3>{title}</H3>
+      <P>{plot}</P>
+    </Container>
+  )
+}
+
+export default Card
